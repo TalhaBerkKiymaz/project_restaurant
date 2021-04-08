72a2eb72ed5928bace24dc6eb7ca19cedcdcce19
Ahmet <leaforewer.net@gmail.com>
2021-04-08 23:42:08 +0200

    aq bu ne

---
+++ bestellen.php
@@ -0,0 +1,126 @@
+<div class="banner-menu">
+    <div>
+        <!-- <img src="./img/menu-banner.png" width="1920" height="1080" alt="Menu Banner"> -->
+    </div>
+</div>
+<div class="container">
+    <div class="main-menu row align-items-start">
+        <div class="col-12">
+            <h3 class="main-menu-h3">Menu
+                <hr>
+            </h3>
+        </div>
+        <div class="category col-4">
+
+            <ul class="menu-group">
+                <h5>Category</h5>
+                <li class="menu-list">
+                    <a href="#cat1">Popular lunches
+                        <hr>
+                    </a>
+                </li>
+                <li class="menu-list">
+                    <a href="">Sushi
+                        <hr>
+                    </a>
+                </li>
+                <li class="menu-list">
+                    <a href="">Sashimi
+                        <hr>
+                    </a>
+                </li>
+                <li class="menu-list">
+                    <a href="">Eggs & Sandwiches
+                        <hr>
+                    </a>
+                </li>
+                <li class="menu-list">
+                    <a href=""> Salads
+                        <hr>
+                    </a>
+                </li>
+                <li class="menu-list">
+                    <a href="">Kids
+                        <hr>
+                    </a>
+                </li>
+                <li class="menu-list">
+                    <a href="">White Wine
+                        <hr>
+                    </a>
+                </li>
+                <li class="menu-list">
+                    <a href="">Red Wine
+                        <hr>
+                    </a>
+                </li>
+                <li class="menu-list">
+                    <a href="">Rosê Wine
+                        <hr>
+                    </a>
+                </li>
+                <li class="menu-list">
+                    <a href="">Sparkling Wines
+                        <hr>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="menukaart col-4">
+            <!-- php ile databaseden cekerken buraya listeyi echola. -->
+            <div class="menu-cat" id="cat1">
+                <h3 id="category"> Sushi </h3>
+                <ul class="menu-cat-group">
+                    <li class="menu-cat-li">
+                        <a class="menu-cat-item" href="">
+                            <b class="item-name"> Salmon flame Roll</b> <br>
+                            <span class="item-desciption">with flame torched salmon & wasabi sesame</span>
+                        </a>
+                    </li>
+                    <li class="menu-cat-li">
+                        <a class="menu-cat-item" href="">
+                            <b class="item-name"> Salmon flame Roll</b> <br>
+                            <span class="item-desciption">with flame torched salmon & wasabi sesame</span>
+                        </a>
+                    </li>
+                    <li class="menu-cat-li">
+                        <a class="menu-cat-item" href="">
+                            <b class="item-name"> Salmon flame Roll</b> <br>
+                            <span class="item-desciption">with flame torched salmon & wasabi sesame</span>
+                        </a>
+                    </li>
+                    <li class="menu-cat-li">
+                        <a class="menu-cat-item" href="">
+                            <b class="item-name"> Salmon flame Roll</b> <br>
+                            <span class="item-desciption">with flame torched salmon & wasabi sesame</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="menu-cat" id="cat1">
+                <h3 id="category"> Sushi </h3>
+                <ul class="menu-cat-group">
+                    <li class="menu-cat-li">
+                        <a class="menu-cat-item" href="">
+                            <b class="item-name"> Salmon flame Roll</b> <br>
+                            <span class="item-desciption">with flame torched salmon & wasabi sesame</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="menu-cat" id="cat1">
+                <h3 id="category"> Sushi </h3>
+                <ul class="menu-cat-group">
+                    <li class="menu-cat-li">
+                        <a class="menu-cat-item" href="">
+                            <b class="item-name"> Salmon flame Roll</b> <br>
+                            <span class="item-desciption">with flame torched salmon & wasabi sesame</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="basket col-4"></div>
+    </div>
+</div>
