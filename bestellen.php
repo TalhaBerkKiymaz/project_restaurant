c38c489442e06753982300a584f3fc4ae1cb6c30
Ahmet <leaforewer.net@gmail.com>
2021-04-10 18:46:47 +0200

    bestteling pagina structuur opgemaakt

--- bestellen.php
+++ bestellen.php
@@ -1,16 +1,14 @@
 <div class="banner-menu">
-    <div>
-        <!-- <img src="./img/menu-banner.png" width="1920" height="1080" alt="Menu Banner"> -->
-    </div>
+    <img src="./img/banner1.jpg" width="1900" height="380" alt="Menu Banner">
 </div>
-<div class="container">
+<div class="container-fluid">
     <div class="main-menu row align-items-start">
         <div class="col-12">
             <h3 class="main-menu-h3">Menu
                 <hr>
             </h3>
         </div>
-        <div class="category col-4">
+        <div class="category col-3 ">
 
             <ul class="menu-group">
                 <h5>Category</h5>
@@ -66,7 +64,7 @@
                 </li>
             </ul>
         </div>
-        <div class="menukaart col-4">
+        <div class="menukaart col-3 offset-1">
             <!-- php ile databaseden cekerken buraya listeyi echola. -->
             <div class="menu-cat" id="cat1">
                 <h3 id="category"> Sushi </h3>
@@ -121,6 +119,50 @@
             </div>
         </div>
 
-        <div class="basket col-4"></div>
+        <div class="basket col-3 offset-1">
+            <div class="basket-title">
+                <h2>Jouw Bestelling</h2>
+            </div>
+            <div class="basket-content">
+                <div id="products">
+                    <div class="cart-row">
+                        <div class="cart-meal-edit-buttons">
+                            <div class="glyphicon glyphicon-plus-sign"></div>
+                            <div class="glyphicon glyphicon-minus-sign"></div>
+                        </div>
+                        <span class="cart-meal-amount">1x </span>
+                        <span class="cart-meal-name">Salmon Flame Roll</span>
+                        <span class="cart-sum-price">€ 8,00</span>
+
+                    </div>
+                </div>
+                <hr>
+                <div id="sum">
+                    <!-- subtotaal -->
+                    <div class="cart-row">
+                        <span class="cart-sum-name grey">Subtotaal</span>
+                        <span class="cart-sum-price">€ 8,00</span>
+                    </div>
+                    <!-- bezorgkosten -->
+                    <div class="cart-row">
+                        <span class="cart-sum-name grey">Bezorgkosten:</span>
+                        <span class="cart-sum-price">€ 2,00</span>
+                    </div>
+                    <!-- Totaal -->
+                    <div class="cart-row">
+                        <span class="cart-sum-name">Totaal</span>
+                        <span class="cart-sum-price">€ 10,00</span>
+                    </div>
+                    <div class="cart-row-extra"><br>
+                        <span class="cart-sum-name"><small>Gratis bezorging vanaf € 15,00</small></span>
+
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="basket-afrekenen">
+            <a class="btn btn-dark" href="#" role="button">ORDER</a>
+            </div>
+        </div>
     </div>
 </div>
--- index.php
+++ index.php
@@ -16,7 +16,9 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="./css/style.css">
 
   <link rel="shortcut icon" href="./img/icons/favicon.png" type="image/x-icon">
