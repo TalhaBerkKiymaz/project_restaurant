1a259511a67d8ef4c683e2c34374d7de95431e27
Vince <vincevandoorn@gmail.com>
2021-06-22 10:03:41 +0200

    reserveerpanel

---
+++ reserveerpanel.php
@@ -0,0 +1,22 @@
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">Datum/Tijd</th>
+            <th scope="col">Aantal gasten</th>
+            <th scope="col">Email</th>
+            <th scope="col"></th>
+        </tr>
+    </thead>
+    <input type="date">
+  </div>
+
+    <tbody>
+        <tr>
+            <th scope="row">22/6/2021 15:00</th>
+            <td>3</td>
+            <td>vince@test.nl</td>
+            <td><button type="button" class="btn btn-primary">Annuleren</button></td>
+            
+        </tr>
+    </tbody>
+</table>
